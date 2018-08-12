cca6f17fc9cf9ff342c8e981b8349af5173e2f38
darnell ghee <jasmine.blackmon@gmail.com>
2018-08-12 14:49:30 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
-def using_include(array, element)
-
+def using_include (array, element)
+array.include?(element)
 end
 
-def using_sort(array)
 
+def using_sort(array)
+#sort.include?(array)
 end
 
 def using_reverse(array)
-
+array.
 end
 
 def using_first(array)
-
+array.
 end
 
 def using_last(array)
-
+array.
 end
 
 def using_size(array)
-
+array.
 end
+
+
+array_methods 
